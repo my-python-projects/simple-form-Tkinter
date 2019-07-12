dad5616a8beba51714ec44e5dbe40fd0f89e136d
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-07-12 16:57:12 -0300

    add date time

--- Register_Control.py
+++ Register_Control.py
@@ -1,14 +1,22 @@
 from Person import Person
+from datetime import date
 
 class Register_Control:
     def __init__(self, name, email, password):
-        self.person = Person(name, email, password)
+        self.get_date()
 
+        self.person = Person(name, email, password)
         self.name = 'Name: {}'.format(self.person.get_name())
         self.email = 'Email: {}'.format(self.person.get_email())
         self.password = 'Password: {}'.format(self.person.get_password())
 
         self.print_console()
+
+    def get_date(self):
+        self.current_date = date.today()
+
+        self.text_date = '{}_{}_{}'.format(self.current_date.day, self.current_date.month, self.current_date.year)
+        self.text_time_data = 'Date created: {}'.format(self.current_date.strftime('%d/%m/%Y %H:%M'))
 
 
     def print_console(self):
@@ -22,14 +30,17 @@ class Register_Control:
 
     def save(self):
         try:
-            self.file_name = '{}.txt'.format(self.person.get_name())
 
+            self.file_name = '{}_{}.txt'.format(self.person.get_name(),self.text_date)
             self.file = open(self.file_name, 'w')
+
             self.file.write(self.name)
             self.file.write('\n')
             self.file.write(self.email)
             self.file.write('\n')
             self.file.write(self.password)
+            self.file.write('\n')
+            self.file.write(self.text_time_data)
 
         except:
             print('Error saving data')
