041837ed65d440051db44ef86d2f33015f99a43c
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-07-12 15:39:49 -0300

    reorganized the class

--- Register_Form.py
+++ Register_Form.py
@@ -2,24 +2,16 @@ import tkinter as tk
 from Register_Control import Register_Control
 
 class Register_Form:
-
-    def __createLabel(self, txt, y):
-        self.label = tk.Label(self.window, text=txt)
-        self.label.place(x = 100, y = y)
-
-    def __createInput(self, y):
-        self.input = tk.Entry(self.window)
-        self.input.place(x = 100, y = y)
-        return self.input
-
-    def __createBtnSubmit(self):
-        self.button = tk.Button(self.window, text = 'Submit', width = 6, background = 'green', command = self.get_input_data)
-        self.button.place(x=100, y=233)
+    def __init__(self):
+        self.__createView()
 
 
-    def get_input_data(self):
-        self.control = Register_Control(self.inputName.get(), self.inputEmail.get(), self.inputPassword.get())
-        self.respost = self. __createLabel('User created successfully', 40)
+    def __createView(self):
+        self.window = tk.Tk()
+        self.window.geometry('400x300+200+200')
+        self.window.title('JProgram - Simple Registration Form')
+        self.__createForm()
+        self.window.mainloop()
 
 
     def __createForm(self):
@@ -34,9 +26,23 @@ class Register_Form:
 
         self.__createBtnSubmit()
 
-    def main(self):
-        self.window = tk.Tk()
-        self.window.geometry('400x300+200+200')
-        self.window.title('JProgram - Simple Registration Form')
-        self.__createForm()
-        self.window.mainloop()
+
+    def __createLabel(self, txt, y):
+        self.label = tk.Label(self.window, text=txt)
+        self.label.place(x = 100, y = y)
+
+
+    def __createInput(self, y):
+        self.input = tk.Entry(self.window)
+        self.input.place(x = 100, y = y)
+        return self.input
+
+
+    def __createBtnSubmit(self):
+        self.button = tk.Button(self.window, text = 'Submit', width = 6, background = 'green', command = self.get_input_data)
+        self.button.place(x=100, y=233)
+
+
+    def get_input_data(self):
+        self.control = Register_Control(self.inputName.get(), self.inputEmail.get(), self.inputPassword.get())
+        self.respost = self. __createLabel('User created successfully', 40)
