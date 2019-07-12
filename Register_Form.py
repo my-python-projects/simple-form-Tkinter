2b7d07f762f03ffec09e372498dc15f377b79435
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-07-12 11:18:26 -0300

    applying encapsulation

--- Register_Form.py
+++ Register_Form.py
@@ -3,46 +3,40 @@ from Register_Control import Register_Control
 
 class Register_Form:
 
-    def createLabel(self, txt, y):
+    def __createLabel(self, txt, y):
         self.label = tk.Label(self.window, text=txt)
         self.label.place(x = 100, y = y)
 
-    def createInput(self, y):
+    def __createInput(self, y):
         self.input = tk.Entry(self.window)
         self.input.place(x = 100, y = y)
         return self.input
 
-    def createBtnSubmit(self):
-        self.button = tk.Button(self.window, text = 'Submit', width = 6, background = 'green', command = self.getValInputs)
+    def __createBtnSubmit(self):
+        self.button = tk.Button(self.window, text = 'Submit', width = 6, background = 'green', command = self.get_input_data)
         self.button.place(x=100, y=233)
 
 
-    def getValInputs(self):
-        self.control = Register_Control()
-        self.control.createPerson(self.inputName.get(), self.inputEmail.get(), self.inputPassword.get())
-        self.alert()
+    def get_input_data(self):
+        self.control = Register_Control(self.inputName.get(), self.inputEmail.get(), self.inputPassword.get())
+        self.respost = self. __createLabel('User created successfully', 40)
 
 
-    def alert(self):
-        self.respost = tk.Label(self.window, text = 'User created successfully')
-        self.respost.place(x=100, y=40)
+    def __createForm(self):
+        self.__createLabel('Your Name: ', 80)
+        self.inputName = self.__createInput(100)
 
+        self.__createLabel('Email: ', 130)
+        self.inputEmail = self.__createInput(150)
 
-    def createForm(self):
-        self.createLabel('Your Name: ', 80)
-        self.inputName = self.createInput(100)
+        self.__createLabel('Password: ', 180)
+        self.inputPassword = self.__createInput(200)
 
-        self.createLabel('Email: ', 130)
-        self.inputEmail = self.createInput(150)
-
-        self.createLabel('Password: ', 180)
-        self.inputPassword = self.createInput(200)
-
-        self.createBtnSubmit()
+        self.__createBtnSubmit()
 
     def main(self):
         self.window = tk.Tk()
         self.window.geometry('400x300+200+200')
         self.window.title('JProgram - Simple Registration Form')
-        self.createForm()
+        self.__createForm()
         self.window.mainloop()
