e53cd653457e4227c57e849c63dbbc68852e2367
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-07-11 23:19:25 -0300

    add method save

--- Register_Control.py
+++ Register_Control.py
@@ -9,8 +9,24 @@ class Register_Control:
 
 
     def printVal(self):
-        print(
-            '\n Name: ', self.person.getName(),
-            '\n Email: ', self.person.getEmail(),
+
+        print('\n Name: ', self.person.getName(), \
+            '\n Email: ', self.person.getEmail(), \
             '\n Password: ', self.person.getSenha()
         )
+        self.save()
+
+    def save(self):
+        try:
+            self.name = 'Name: {}'.format(self.person.getName())
+            self.arquivo = '{}.txt'.format(self.person.getName())
+            self.f = open(self.arquivo, 'w')
+            self.f.write(self.name)
+            self.f.write('\n')
+
+        except:
+            print('Erro')
+
+        finally:
+            self.f.close()
+            print('Dados salvos com sucesso')
